64d799c94c41074711a668967a72257af2a7075a
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2021-08-13 08:56:45 +0700

    Banner

--- resources/views/admin/appadmin.blade.php
+++ resources/views/admin/appadmin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>    
+    <title>Admin {{session('divisi')}} Al-Mu'awanah</title>    
 
     <!-- General CSS Files -->
   <link rel="stylesheet" href="{{ asset('adminAssets/modules/bootstrap/css/bootstrap.min.css') }}">
--- resources/views/admin/banner.blade.php
+++ resources/views/admin/banner.blade.php
@@ -10,12 +10,15 @@
       <div class="section-body">
         <h2 class="section-title">Halaman Awal</h2>
         
-        <div class="row">
-          <div class="col-12">
+        <div class="row">          
+          <div class="col-12 collapse multi-collapse" id="newbanner" >
             <div class="card">
               <!-- Header Table -->
               <div class="card-header">
-                <h4>Banner 1</h4>              
+                <h4>Tambah Banner</h4>
+                <div class="card-header-action">
+                  <button class="btn btn-icon btn-danger" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="newbanner"><i class="fas fa-times"></i></button>
+                </div>
               </div>
               <div class="card-body">
                 <!-- Banner Form -->
@@ -30,7 +33,7 @@
                 <div class="form-group row mb-4">
                   <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Content</label>
                   <div class="col-sm-12 col-md-7">
-                    <textarea class="summernote-simple"></textarea>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                   </div>
                 </div>
                 <!-- Thumbnail -->
@@ -47,12 +50,14 @@
               <!-- Buttons -->
               <div class="card-footer text-right">
                   <div class="buttons">
-                      <button class="btn btn-danger">Hapus Banner</button>
                       <button class="btn btn-success">Simpan Banner</button>
                   </div>
               </div>
             </div>
           </div>
+          <div class="col-12 text-center multi-collapse collapse show" >    
+            <button type="button" class="btn btn-primary" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="newbanner"><i class="fas fa-plus" style="font-size: 3rem"></i></button>    
+          </div>         
         </div>            
       </div>
     </section>
