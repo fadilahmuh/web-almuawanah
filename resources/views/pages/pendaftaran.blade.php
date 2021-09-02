fd186988523cd194e3ec3a198d2501aa98c28278
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2021-09-02 09:35:04 +0700

    Form Pendaftaran

--- resources/views/pages/pendaftaran.blade.php
+++ resources/views/pages/pendaftaran.blade.php
@@ -11,62 +11,143 @@
     </div>
   </section>
 
-  <section class="ftco-section">
-    <div class="container">
-      <div class="heading_container">
+  <section class="both-margin">
+      <div class="heading_container" data-aos="fade-right">
         <h3>Formulir Pendaftaran</h3>          
       </div>
-      <div class="formulir post-entry-1">
-        <form>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputEmail4">Email</label>
-              <input type="email" class="form-control" id="inputEmail4">
-            </div>
-            <div class="form-group col-md-6">
-              <label for="inputPassword4">Password</label>
-              <input type="password" class="form-control" id="inputPassword4">
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="inputAddress">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-          </div>
-          <div class="form-group">
-            <label for="inputAddress2">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-          </div>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputCity">City</label>
-              <input type="text" class="form-control" id="inputCity">
-            </div>
-            <div class="form-group col-md-4">
-              <label for="inputState">State</label>
-              <select id="inputState" class="form-control">
-                <option selected>Choose...</option>
-                <option>...</option>
-              </select>
-            </div>
-            <div class="form-group col-md-2">
-              <label for="inputZip">Zip</label>
-              <input type="text" class="form-control" id="inputZip">
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck">
-              <label class="form-check-label" for="gridCheck">
-                Check me out
-              </label>
-            </div>
-          </div>
-          <div class="col-12 text-end">
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
-          <!-- <button type="submit" class="btn btn-primary">Sign in</button> -->
-        </form>
+      <div class="container">
+      <!-- MultiStep Form -->
+      <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-8 col--12">
+            <form id="msform">
+                <!-- progressbar -->
+                <ul id="progressbar" data-aos="fade-up">
+                    <li class="active">Personal Details</li>
+                    <li>Social Profiles</li>
+                    <li>Account Setup</li>
+                </ul>
+                <!-- fieldsets -->
+                <fieldset data-aos="fade-up" data-aos-delay="200">
+                    <h2 class="fs-title">Personal Details</h2>
+                    <h3 class="fs-subtitle">Tell us something more about you</h3>
+                    <input type="text" name="fname" placeholder="First Name"/>
+                    <input type="text" name="lname" placeholder="Last Name"/>
+                    <input type="text" name="phone" placeholder="Phone"/>
+                    <input type="button" name="next" class="next action-button" value="Next"/>
+                </fieldset>
+                <fieldset>
+                    <h2 class="fs-title">Social Profiles</h2>
+                    <h3 class="fs-subtitle">Your presence on the social network</h3>
+                    <input type="text" name="twitter" placeholder="Twitter"/>
+                    <input type="text" name="facebook" placeholder="Facebook"/>
+                    <input type="text" name="gplus" placeholder="Google Plus"/>
+                    <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                    <input type="button" name="next" class="next action-button" value="Next"/>
+                </fieldset>
+                <fieldset>
+                  <h2 class="fs-title">Create your account</h2>
+                  <h3 class="fs-subtitle">Fill in your credentials</h3>
+                  <input type="text" name="email" placeholder="Email"/>
+                  <input type="password" name="pass" placeholder="Password"/>
+                  <input type="password" name="cpass" placeholder="Confirm Password"/>
+                  <input type="button" name="previous" class="previous action-button-previous" value="Previous"/>
+                  <input type="submit" name="submit" class="submit action-button" value="Submit"/>
+              </fieldset>
+            </form>
+            <!-- link to designify.me code snippets -->
+            
+            <!-- /.link to designify.me code snippets -->
+        </div>
       </div>
-    </div>
+<!-- /.MultiStep Form -->
+      </div>
   </section>
+@endsection
+
+@section('scriptpage')
+<script>
+  
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	//activate next step on progressbar using the index of next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	//show the next fieldset
+	next_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'absolute'
+      });
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();  
+	previous_fs = $(this).parent().prev();
+	
+	//de-activate current step on progressbar
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//show the previous fieldset
+	previous_fs.show(); 
+	//hide the current fieldset with style
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+      previous_fs.css({'position': 'relative'});
+		},     
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".submit").click(function(){
+	return false;
+})
+</script>
 @endsection
